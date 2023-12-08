9fffa0a861a7d380ade4aa6f03e62b75a6d22fe3
JunHaooo <strongboyljh@gmail.com>
2023-12-08 17:15:33 +0800

    Created a 16x16 grid with DOMS

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const gridContainer = document.querySelector('#grud-container');
-const grid = document.createElement('div');
+const gridContainer = document.querySelector('#grid-container');
+gridContainer.classList.add('grid-container');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const col = document.createElement('div');
+        col.classList.add('grid-item');
+        gridContainer.appendChild(col);
+    }
+}
