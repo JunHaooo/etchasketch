34de3b34ef0997dca012e449d807f3a74029077a
JunHaooo <strongboyljh@gmail.com>
2023-12-11 18:38:59 +0800

    utilised event delegation to better help with response time of project. Works!

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const gridContainer = document.querySelector('#grid-container');
 gridContainer.classList.add('grid-container');
 
+let idCounter = 1;
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        const col = document.createElement('div');
-        col.classList.add('grid-item');
+        let col = document.createElement('div');
+        col.setAttribute('class', 'grid-item')
+        col.setAttribute('id',`gridItem${idCounter}`);
+        idCounter++;
         gridContainer.appendChild(col);
     }
 }
 
-const gridItems = document.querySelectorAll('.grid-item');
-let isMouseDown = false;
+let isMouseDown = false ;
 
-gridItems.forEach(gridItem => {
-    gridItem.addEventListener('mousedown', () => {
+gridContainer.addEventListener('mousedown', (event) => {
+    let target = event.target;
+    if (target.classList.contains('grid-item')) {
         isMouseDown = true;
-        gridItem.style.backgroundColor = 'blue'; // Change the color as desired
-      });
+        target.style.backgroundColor = 'blue';
+    }
+});
 
-    document.addEventListener('mousemove', (event) => {
+gridContainer.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+document.addEventListener('mousemove', (event) => {
     if (isMouseDown) {
-        const hoveredElement = event.target;
-        if (hoveredElement.classList.contains('grid-item')) {
-        hoveredElement.style.backgroundColor = 'blue'; // Change the color as desired
+        let target = event.target;
+        if (target.classList.contains('grid-item')) {
+        target.style.backgroundColor = 'blue'; // Change the color as desired
         }
     }
     
     document.addEventListener('mouseup', () => {
         isMouseDown = false;
-    }
-    )
+    })
 });
-
-});
-  
 
