1dba96a504b4dab9cee04f12a5b8e6c74d945cea
JunHaooo <strongboyljh@gmail.com>
2023-12-12 21:39:13 +0800

    Allow for users to create grids of varying pixels

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+let size = 16; 
+size = Number(prompt("How big do you want your grid to be (max 100)"));
+
 const gridContainer = document.querySelector('#grid-container');
-gridContainer.classList.add('grid-container');
 
 let idCounter = 1;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < size; i++) {
+    let row = document.createElement('div');
+    row.setAttribute('id','row');
+    gridContainer.appendChild(row);
+    for (let j = 0; j < size; j++) {
         let col = document.createElement('div');
         col.setAttribute('class', 'grid-item')
         col.setAttribute('id',`gridItem${idCounter}`);
         idCounter++;
-        gridContainer.appendChild(col);
+        row.appendChild(col);
     }
 }
+
+//gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+//gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+const cellSize = 400 / size; // Adjust container width (400) as needed
+const gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach(item => {
+    item.style.width = `${cellSize}px`;
+    item.style.height = `${cellSize}px`;
+});
 
 let isMouseDown = false ;
 
@@ -27,16 +43,18 @@ gridContainer.addEventListener('mouseup', () => {
     isMouseDown = false;
 });
 
-document.addEventListener('mousemove', (event) => {
+gridContainer.addEventListener('mouseleave', () => {
+    isMouseDown = false;
+});
+
+gridContainer.addEventListener('mousemove', (event) => {
     if (isMouseDown) {
         let target = event.target;
         if (target.classList.contains('grid-item')) {
-        target.style.backgroundColor = 'blue'; // Change the color as desired
+            target.style.backgroundColor = 'blue';
         }
     }
-    
-    document.addEventListener('mouseup', () => {
-        isMouseDown = false;
-    })
 });
+
+
 
